21138b49341f4d8eff98d33291ed52f063b7c36b
Bobbie Deniker <2250620+bdeniker@users.noreply.github.com>
2022-09-14 00:11:41 +0100

    Add [broken] problem dropdowns you can swipe to remove

--- screens/diagnose/index.js
+++ screens/diagnose/index.js
@@ -1,12 +1,117 @@
 import React, {useState} from 'react';
-import {Alert, StyleSheet, Text, View} from 'react-native';
+import {
+  Alert,
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {Slider} from '@miblanchard/react-native-slider';
+import uuid from 'react-native-uuid';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {SwipeListView} from 'react-native-swipe-list-view';
 import BigButton from '../../components/BigButton';
+import Row from '../../components/Row';
+import problemJSON from '../../resources/problems.json';
+
+const rowTranslateAnimatedValues = {};
 
 function Diagnose() {
+  const [problemNames] = useState(problemJSON.map(p => ({label: p, value: p})));
+  const [problems, setProblems] = useState([]);
   const [surgeries, setSurgeries] = useState(0);
+
+  function openDropdown(idToOpen, isOpen) {
+    setProblems(
+      problems.map(problem =>
+        problem.id === idToOpen ? {...problem, open: isOpen} : problem,
+      ),
+    );
+    console.log(JSON.stringify(problems));
+  }
+  function updateProblem(idToReplace, newProblem) {
+    setProblems(
+      problems.map(problem =>
+        problem.id === idToReplace ? {...problem, value: newProblem} : problem,
+      ),
+    );
+  }
+
+  function renderItem({item}) {
+    return (
+      <Animated.View
+        style={[
+          styles.rowFrontContainer,
+          {
+            height: rowTranslateAnimatedValues[item.key].interpolate({
+              inputRange: [0, 1],
+              outputRange: [0, 50],
+            }),
+          },
+        ]}>
+        <DropDownPicker
+          key={`dropdown-${item.id}`}
+          open={item.open}
+          value={item.value}
+          items={problemNames}
+          setOpen={isOpen => openDropdown(item.id, isOpen)}
+          setValue={newProblem => updateProblem(item.id, newProblem)}
+          searchable={true}
+        />
+      </Animated.View>
+    );
+  }
+
+  const onSwipeValueChange = swipeData => {
+    const {key, value} = swipeData;
+    if (value < -Dimensions.get('window').width && !this.animationIsRunning) {
+      this.animationIsRunning = true;
+      Animated.timing(rowTranslateAnimatedValues[key], {
+        toValue: 0,
+        duration: 200,
+      }).start(() => {
+        const newData = [...problems];
+        const prevIndex = problems.findIndex(item => item.key === key);
+        newData.splice(prevIndex, 1);
+        setProblems(newData);
+        this.animationIsRunning = false;
+      });
+    }
+  };
+
   return (
     <View style={styles.homeView}>
+      <SwipeListView
+        disableRightSwipe
+        data={problems}
+        renderItem={renderItem}
+        renderHiddenItem={() => (
+          <View style={styles.rowBack}>
+            <Text style={styles.textBack}>⬅️ Swipe to Delete</Text>
+          </View>
+        )}
+        rightOpenValue={-Dimensions.get('window').width}
+        onSwipeValueChange={onSwipeValueChange}
+        useNativeDriver={false}
+      />
+      <Row>
+        <BigButton
+          title="Add Problem"
+          onPress={() => {
+            let newId = uuid.v4();
+            setProblems([
+              ...problems,
+              {value: null, open: false, id: newId, key: newId},
+            ]);
+            rowTranslateAnimatedValues[newId] = new Animated.Value(1);
+          }}
+        />
+        <BigButton
+          title="Add wound card"
+          onPress={() => console.log(problems)}
+        />
+      </Row>
       <Text>Previous surgeries: {surgeries}</Text>
       <Slider
         value={surgeries}
@@ -15,8 +120,9 @@ function Diagnose() {
         onValueChange={value => setSurgeries(value)}
       />
       <BigButton
-        title="Start X card problem"
+        title={`Start ${problems.length} card problem`}
         onPress={() => Alert.alert('To do: actual function 🤷')}
+        disabled={problems.length < 1} // Y U no work?!
       />
     </View>
   );
@@ -25,6 +131,36 @@ function Diagnose() {
 const styles = StyleSheet.create({
   homeView: {
     margin: 10,
+  },
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  rowBack: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingRight: 15,
+  },
+  textBack: {
+    // color: 'red',
+    fontWeight: 'bold',
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: 'red',
+    right: 0,
   },
 });
 
