88ddd37c00c8fe19583c804803fd5d555e873371
Bobbie Deniker <boryana.deniker@gmail.com>
2026-06-13 22:11:36 +0100

    move Category to always show

--- views/diagnosis-view.tsx
+++ views/diagnosis-view.tsx
@@ -3,13 +3,11 @@ import Body from '@/components/body'
 import Heading from '@/components/heading'
 import Title from '@/components/title'
 import { StyleSheet, View } from 'react-native'
-import Subtitle from '../components/subtitle'
 
 export default function DiagnosisView({ wound }: { wound: Wound }) {
     return (
         <View style={styles.diagnosis}>
             <Title titleText={wound.name} />
-            <Subtitle subtitleText={wound.category} />
             <Heading headingText="Diagnosis" />
             <Body bodyText={wound.diagnosis} />
             {wound.mechanics && <Body isMechanics bodyText={wound.mechanics} />}
--- views/injury-view.tsx
+++ views/injury-view.tsx
@@ -1,6 +1,7 @@
 import { Wound } from '@/assets/wounds'
 import Body from '@/components/body'
 import Heading from '@/components/heading'
+import Subtitle from '@/components/subtitle'
 import Title from '@/components/title'
 import { StyleSheet, View } from 'react-native'
 
@@ -8,6 +9,7 @@ export default function InjuryView({ wound }: { wound: Wound }) {
     return (
         <View style={styles.injury}>
             <Title titleText={`${wound.name} ${wound.isWhite ? '⚪' : '⚫'}`} />
+            <Subtitle subtitleText={wound.category} />
             <Heading headingText="Symptoms" />
             <Body bodyText={wound.symptoms} />
             <Heading headingText="Until Treated" />
